d9769c7585017cf2ec12f4d410c18223acef4038
RoVsEk23 <rohitk23@gmail.com>
2020-10-22 13:42:16 +0000

    changed arrangement on header

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+  console.log("Test 123");
+  // navUL.classList.toggle('show');
+  navUL.classList.toggle("show");
+});
